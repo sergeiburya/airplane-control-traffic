288ebb74ba7ad1aac5a6601455e1697a4909ff22
Serhii Buria <sergeiburya@gmail.com>
2023-01-27 13:01:21 +0200

    initial

---
+++ src/main/java/com/example/airplanecontroltraffic/AirplaneControlTrafficApplication.java
@@ -0,0 +1,13 @@
+package com.example.airplanecontroltraffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirplaneControlTrafficApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirplaneControlTrafficApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/airplanecontroltraffic/AirplaneControlTrafficApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airplanecontroltraffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirplaneControlTrafficApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
